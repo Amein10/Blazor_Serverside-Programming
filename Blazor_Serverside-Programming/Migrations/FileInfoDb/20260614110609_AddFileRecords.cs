f8ff662ba6b4e8989ffd49943888eecd154d58ff
Amein <amein@gmail.com>
2026-06-14 13:19:13 +0200

    Add admin file upload with SQLite metadata storag

---
+++ Blazor_Serverside-Programming/Migrations/FileInfoDb/20260614110609_AddFileRecords.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor_Serverside_Programming.Migrations.FileInfoDb
+{
+    /// <inheritdoc />
+    public partial class AddFileRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
+    
