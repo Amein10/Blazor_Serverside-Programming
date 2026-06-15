57a6f0ac843c44164fa5007397e84a635fbfbeb0
Amein <amein@gmail.com>
2026-06-15 20:24:48 +0200

    Færdiggjort: Beskyttelse mod Man-in-the-Middle ved offentlig nøgle

--- Blazor_Serverside-Programming/Services/RsaHandler.cs
+++ Blazor_Serverside-Programming/Services/RsaHandler.cs
@@ -9,6 +9,31 @@ public class RsaHandler
     public RsaHandler()
     {
         _rsa = RSA.Create(2048);
+
+        var keyFolder = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "Keys");
+
+        Directory.CreateDirectory(keyFolder);
+
+        var privateKeyPath = Path.Combine(
+            keyFolder,
+            "rsa_private_key.txt");
+
+        if (File.Exists(privateKeyPath))
+        {
+            var privateKeyBase64 = File.ReadAllText(privateKeyPath);
+            var privateKeyBytes = Convert.FromBase64String(privateKeyBase64);
+
+            _rsa.ImportPkcs8PrivateKey(privateKeyBytes, out _);
+        }
+        else
+        {
+            var privateKeyBytes = _rsa.ExportPkcs8PrivateKey();
+            var privateKeyBase64 = Convert.ToBase64String(privateKeyBytes);
+
+            File.WriteAllText(privateKeyPath, privateKeyBase64);
+        }
     }
 
     public string GetPublicKey()
